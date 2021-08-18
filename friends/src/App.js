16c481ea54702cca79ae9b8f9d7dfeeefd58a138
Andrew Cummings <andrew.amalee@gmail.com>
2021-08-17 21:05:04 -0700

    working on adding friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
+import FriendsList from "./components/FriendsList";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -11,10 +14,23 @@ function App() {
           <li>
             <Link to="/login">Login</Link>
           </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+          <li>
+            {localStorage.getItem("token") && (
+              <div>
+                <Link to="/protected">Protected Page</Link>
+              </div>
+            )}
+          </li>
         </ul>
 
         <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <Route path="/logout" component={Logout} />
           <Route path="/login" component={Login} />
+          <Route path="/" component={Login} />
         </Switch>
       </div>
     </Router>
